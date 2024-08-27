67961b46e6a2b1db6c9ff23ee1a82d3a3562c45f
rywblack <ry.black@me.com>
2024-08-27 01:37:01 -0500

    added word count, sorted list of each character and its count

---
+++ main.py
@@ -0,0 +1,37 @@
+from operator import itemgetter
+def getText():
+    with open("books/frankenstein.txt") as f:
+        return f.read()
+
+def getWordCount(text):
+    words = text.split()
+    wc = len(words)
+    return wc
+
+def getCharCount(text):
+    characters = list(text.lower())
+    characterCount = {}
+    for c in characters:
+        if c not in characterCount:
+            characterCount[c] = 1
+        else:
+            characterCount[c] += 1
+    dictList = []
+    for char in characterCount:
+        if char.isalpha():
+            dictList.append({'name': char, 'value': characterCount[char]})
+
+    sortedDict = sorted(dictList, key=itemgetter('value'), reverse=True)
+
+    for d in sortedDict:
+        print(f"The '{d['name']}' character was found {d['value']} times")
+
+def main():
+    text = getText()
+    wordCount = getWordCount(text)
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{wordCount} words found in doc")
+    getCharCount(text)
+    print("--- End report ---")
+
+main()
